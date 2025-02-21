e389483739d28ddc0c8f3541cfda5c0102583411
sapphiriza <sapphiriza@gmail.com>
2025-02-21 14:34:50 -0800

    Version 1.0.1 & Version 1.0.2: Byproducts.

--- base-data-updates.lua
+++ base-data-updates.lua
@@ -16,15 +16,43 @@ for _,asteroid_type in pairs(asteroid_types) do
   end
 end
 
+local new_byproducts = {}
 local metallic_byproduct = settings.startup["cupric-asteroids-metallic-byproduct"].value
 if metallic_byproduct ~= "copper-ore" then
+  new_byproducts[1] = metallic_byproduct
   frep.replace_result("advanced-metallic-asteroid-crushing", "copper-ore", metallic_byproduct)
 end
 
 local metallic_triproduct = settings.startup["cupric-asteroids-metallic-triproduct"].value
 if metallic_triproduct ~= "none" and metallic_triproduct ~= metallic_byproduct then
+  new_byproducts[2] = metallic_triproduct
   local byproduct_result = frep.scale_result("advanced-metallic-asteroid-crushing", metallic_byproduct, {amount=0.5})
   frep.add_result("advanced-metallic-asteroid-crushing", {type="item", name=metallic_triproduct, amount=byproduct_result.amount})
+end
+
+if #new_byproducts > 0 then
+  local ore_icons = {
+    ["iron-ore"] = {"__base__/graphics/icons/iron-ore.png", "__base__/graphics/icons/iron-ore-2.png", "__base__/graphics/icons/iron-ore-1.png"},
+    ["copper-ore"] = {"__base__/graphics/icons/copper-ore.png", "__base__/graphics/icons/copper-ore-2.png", "__base__/graphics/icons/copper-ore-1.png"},
+    ["uranium-ore"] = {"__base__/graphics/icons/uranium-ore.png", "__base__/graphics/icons/uranium-ore-2.png", "__base__/graphics/icons/uranium-ore-3.png"},
+    ["tungsten-ore"] = {"__space-age__/graphics/icons/tungsten-ore.png", "__space-age__/graphics/icons/tungsten-ore-2.png", "__space-age__/graphics/icons/tungsten-ore-3.png"},
+    ["holmium-ore"] = {"__space-age__/graphics/icons/holmium-ore.png", "__space-age__/graphics/icons/holmium-ore.png", "__space-age__/graphics/icons/holmium-ore.png"},
+    ["stone"] = {"__base__/graphics/icons/stone.png", "__base__/graphics/icons/stone-1.png", "__base__/graphics/icons/stone-3.png"}
+  }
+
+  local recipe = data.raw.recipe["advanced-metallic-asteroid-crushing"]
+  recipe.icons = {}
+  local has_triproduct = (new_byproducts[2] ~= nil)
+  local product_1 = has_triproduct and metallic_byproduct or "iron-ore"
+  local product_2 = has_triproduct and new_byproducts[2] or new_byproducts[1]
+
+  recipe.icons = {
+    {icon=ore_icons[product_1][2], icon_size=64, scale=0.25, shift={-8,-8}, draw_background=false},
+    {icon=ore_icons[product_2][2], icon_size=64, scale=0.25, shift={8,-8}, draw_background=false},
+    {icon=ore_icons[product_1][1], icon_size=64, scale=0.25, shift={-8,8}, draw_background=true},
+    {icon=ore_icons[product_2][1], icon_size=64, scale=0.25, shift={8,8}, draw_background=true},
+    {icon="__space-age__/graphics/icons/metallic-asteroid-chunk.png", icon_size=64, scale=0.45, draw_background=true},
+  }
 end
 
 -------------------------------------------------------------------------- Asteroid 
--- settings.lua
+++ settings.lua
@@ -5,14 +5,14 @@ data:extend({
     name = "cupric-asteroids-metallic-byproduct",
     setting_type = "startup",
     allowed_values = {"copper-ore", "uranium-ore", "tungsten-ore", "holmium-ore", "stone"},
-    default_value = "tungsten-ore"
+    default_value = "uranium-ore"
   },
   -- choose a third byproduct for advanced metallic asteroid crushing
   {
     type = "string-setting",
     name = "cupric-asteroids-metallic-triproduct",
     setting_type = "startup",
-    allowed_values = {"none", "copper-ore", "uranium-ore", "tungsten-ore", "holmium-ore", "stone"},
-    default_value = "holmium-ore"
+    allowed_values = {"none", "uranium-ore", "tungsten-ore", "holmium-ore", "stone"},
+    default_value = "stone"
   }
 })
