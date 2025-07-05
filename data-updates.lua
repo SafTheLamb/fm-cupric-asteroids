f7d6779e37ba08af123017a363c83ac50e58b40b
sparkletwist <sparkletwist@gmail.com>
2025-07-04 23:21:00 -0500

    1.5.3 Updates

--- data-updates.lua
+++ data-updates.lua
@@ -1,5 +1,3 @@
-require("prototypes.planet.planet-updates")
-
 require("prototypes.technology-updates")
 
 require("prototypes.compat.bz-ores-updates")
--- prototypes/compat/voidcraft-patch.lua
+++
@@ -1,5 +0,0 @@
-if mods["Voidcraft"] and (mods["Voidcraft"] <= "1.4.19") and data.raw.recipe['s6xvc-cupric-asteroid-chunk'] then
-  data.raw.recipe["s6xvc-cupric-asteroid-chunk"].icons[1].icon = "__cupric-asteroids__/graphics/icons/cupric-asteroid-chunk.png"
-  data.raw.recipe["s6xvc-cupric-asteroid-chunk-flx"].icons[1].icon = "__cupric-asteroids__/graphics/icons/cupric-asteroid-chunk.png"
-  data.raw.recipe["s6xvc-cupric-asteroid-chunk-coh"].icons[1].icon = "__cupric-asteroids__/graphics/icons/cupric-asteroid-chunk.png"
-end
