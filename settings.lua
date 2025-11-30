bab6341571bdb1c411f036a5ffb75b9ac3c5a0de
SafTheLamb <safthelamb@gmail.com>
2025-11-30 14:47:08 -0800

    Crushing Industry already handles stone as a byproduct

--- settings.lua
+++ settings.lua
@@ -66,13 +66,35 @@ if mods["bztin"] then
   })
 end
 
-local function add_byproduct_options(item_name)
-  table.insert(data.raw["string-setting"]["cupric-asteroids-cupric-byproduct"].allowed_values, item_name)
-  table.insert(data.raw["string-setting"]["cupric-asteroids-cupric-triproduct"].allowed_values, item_name)
-  table.insert(data.raw["string-setting"]["cupric-asteroids-metallic-byproduct"].allowed_values, item_name)
-  table.insert(data.raw["string-setting"]["cupric-asteroids-metallic-triproduct"].allowed_values, item_name)
+local function add_byproduct_option(setting_name, item_name)
+  local setting = data.raw["string-setting"][setting_name]
+  if setting and setting.allowed_values then
+    table.insert(setting.allowed_values, item_name)
+  end
 end
 
 if mods["bztitanium"] then
-  add_byproduct_options("titanium-ore")
+  add_byproduct_option("cupric-asteroids-cupric-byproduct", "titanium-ore")
+  add_byproduct_option("cupric-asteroids-cupric-triproduct", "titanium-ore")
+  add_byproduct_option("cupric-asteroids-metallic-byproduct", "titanium-ore")
+  add_byproduct_option("cupric-asteroids-metallic-triproduct", "titanium-ore")
+end
+
+local function remove_byproduct_option(setting_name, item_name)
+  local setting = data.raw["string-setting"][setting_name]
+  if setting and setting.allowed_values then
+    for i,byproduct_name in pairs(setting.allowed_values) do
+      if byproduct_name == item_name then
+        table.remove(setting.allowed_values, i)
+        return
+      end
+    end
+  end
+end
+
+if mods["crushing-industry"] then
+  remove_byproduct_option("cupric-asteroids-cupric-byproduct", "stone")
+  remove_byproduct_option("cupric-asteroids-cupric-triproduct", "stone")
+  remove_byproduct_option("cupric-asteroids-metallic-byproduct", "stone")
+  remove_byproduct_option("cupric-asteroids-metallic-triproduct", "stone")
 end
